893b12c0d2aa1d599332c31b0b055f963af30e9d
ML <kite.616@gmail.com>
2023-06-26 18:01:53 +0200

    Extended Calculator class, project files
    Added the ExtendedCalculatorClass
    Added missing project files

---
+++ src/main/java/org/munzinger/ExtendedCalculator.java
@@ -0,0 +1,19 @@
+package org.munzinger;
+/**
+ * Beispielklasse aus der zweiten Präsenz wiederverwendet für ESA8.
+ *
+ * @author Maximilian Munzinger
+ * @since 1.0
+ */
+public class ExtendedCalculator extends Calculator {
+    /**
+     * Multiplikation
+     *
+     * @param a Erster Paramter
+     * @param b Erster Paramter
+     * @return Multiplikationsergebnis der beiden Parameter
+     */
+    public int modulo(int a, int b) {
+        return a % b;
+    }
+}
